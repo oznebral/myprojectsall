4d8f7d1cfa4b15a8473bf4cc9d1fd354db55499c
Ebral <ebray.ozen@icloud.com>
2020-07-16 11:04:31 +0300

    TrypByWyndhamHotel
    my first web site and it's name

---
+++ web_site/javascript/localize-en.js
@@ -0,0 +1,5 @@
+var $localize = {
+        days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+        months: ['january', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        first_day_of_week: 1, show_select_today: 'Today', lang_clear_date: 'Clear', format: 'd/m/Y'
+    };
